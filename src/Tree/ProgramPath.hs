b964b9cde63427569d52fa2a7683b1e7a1457e99
Ssoulner <delinda.lortie@gmail.com>
2024-09-15 21:59:19 +0200

    While program paths and printing

--- src/Tree/ProgramPath.hs
+++ src/Tree/ProgramPath.hs
@@ -1,50 +1,78 @@
+{-# LANGUAGE InstanceSigs #-}
 module Tree.ProgramPath where
 
 import GCLParser.GCLDatatype
 import GCLParser.Parser ( parseGCLfile )
 
+data UniBinTree u b = Leaf
+  | Uni u (UniBinTree u b)
+  | Bin b (UniBinTree u b) (UniBinTree u b)
+
+type ControlPath = UniBinTree Stmt Expr
+
+instance (Show u, Show b) => Show (UniBinTree u b) where
+  show :: UniBinTree u b -> String
+  show = printTree 0
+
+printTree :: (Show u, Show b) => Int -> UniBinTree u b -> String
+printTree n Leaf               = replicate (n * 2) ' ' ++ "Leaf\n"
+printTree n (Uni u next)       = replicate (n * 2) ' ' ++ show u ++ "\n" ++ printTree n next
+printTree n (Bin b left right) = replicate (n * 2) ' ' ++ show b ++ "\n" ++ indent ++ "< LEFT >\n" ++ printTree (n + 1) left ++ indent ++ "< RIGHT >\n" ++ printTree (n + 1) right
+  where indent = replicate ((n + 1) * 2) ' '
+
 orElse :: Maybe t -> Maybe t -> Maybe t
 orElse val@(Just _) _   = val
 orElse Nothing      def = def
 
-data ControlPath = Basic Stmt (Maybe ControlPath)
-  | Branch Expr ControlPath ControlPath
-  deriving (Show)
+-- data ControlPath = Basic Stmt (Maybe ControlPath)
+--   | Branch Expr ControlPath ControlPath
+--   deriving (Show)
 
 (+:) :: Stmt -> Stmt -> Stmt
-(Seq lstmt1 lstmt2) +: rstmt = 
+(Seq lstmt1 lstmt2) +: rstmt =
   Seq lstmt1 (lstmt2 +: rstmt)
 lstmt +: rstmt         = Seq lstmt rstmt
 
 controlLeaf :: Stmt -> ControlPath
-controlLeaf stmt = Basic stmt Nothing
+controlLeaf stmt = Uni stmt Leaf
 
-extractPaths :: Stmt -> ControlPath
-extractPaths stmt@Skip         = controlLeaf stmt
-extractPaths stmt@(Assert {})  = controlLeaf stmt
-extractPaths stmt@(Assume {})  = controlLeaf stmt
-extractPaths stmt@(Assign {})  = controlLeaf stmt
-extractPaths stmt@(AAssign {}) = controlLeaf stmt
-extractPaths (Block _ stmt)    = extractPaths stmt
+extractPaths :: Int -> Stmt -> ControlPath
+extractPaths 0 _                 = Leaf
+extractPaths _ stmt@Skip         = controlLeaf stmt
+extractPaths _ stmt@(Assert {})  = controlLeaf stmt
+extractPaths _ stmt@(Assume {})  = controlLeaf stmt
+extractPaths _ stmt@(Assign {})  = controlLeaf stmt
+extractPaths _ stmt@(AAssign {}) = controlLeaf stmt
+extractPaths n (Block _ stmt)    = extractPaths n stmt
+
+extractPaths n (IfThenElse cond thenStmt elseStmt) =
+  Bin cond (extractPaths (n - 1) thenStmt) (extractPaths (n - 1) elseStmt)
+extractPaths n (While cond body) = Bin cond (extractPaths (n - 1) body) Leaf
+
+extractPaths n (Seq (Block _ stmt1) stmt2) = extractPaths n (stmt1 +: stmt2)
 
 -- append statements following an if-then-else to both branches
-extractPaths (Seq (IfThenElse cond thenStmt elseStmt) stmt) =
-  Branch cond thenPaths elsePaths
-    where thenPaths = extractPaths $ thenStmt +: stmt
-          elsePaths = extractPaths $ elseStmt +: stmt
+extractPaths n (Seq (IfThenElse cond thenStmt elseStmt) stmt) =
+  Bin cond thenPaths elsePaths
+    where thenPaths = extractPaths (n - 1) (thenStmt +: stmt)
+          elsePaths = extractPaths (n - 1) (elseStmt +: stmt)
 
-extractPaths (Seq stmt1 stmt2) = Basic stmt1 (Just $ extractPaths stmt2)
-extractPaths (IfThenElse cond thenStmt elseStmt) = Branch cond (extractPaths thenStmt) (extractPaths elseStmt)
+extractPaths n (Seq while@(While cond body) stmt) = Bin cond whilePaths exitPaths
+  where whilePaths = extractPaths (n - 1) (body +: while)
+        exitPaths  = extractPaths (n - 1) stmt
 
-extractPaths _ = error "not yet implemented"
+extractPaths n (Seq stmt1 stmt2) = Uni stmt1 (extractPaths (n - 1) stmt2)
+
+extractPaths _ _ = error "not yet implemented"
 -- extractPaths (While cond body) = Branch cond thenStmt elseStmt
 
 toMaybe :: Either l r -> Maybe r
 toMaybe (Right r) = Just r
 toMaybe (Left _)  = Nothing
 
-testExtract :: String -> IO (Maybe ControlPath)
-testExtract file = do
+testExtract :: Int -> String -> IO (Maybe ControlPath)
+testExtract n file = do
   gcl <- parseGCLfile file
-  return $ fmap (extractPaths . stmt) (toMaybe gcl)
+  return $ fmap (extractPaths n . stmt) (toMaybe gcl)
+  -- return $ fmap (extractPaths 1000000 . stmt) (toMaybe gcl)
 
