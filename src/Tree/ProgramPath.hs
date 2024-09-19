eb2f120ef53152016899b130ad4efd39351b60a4
MartijnCBV <49876349+MartijnCBV@users.noreply.github.com>
2024-09-19 16:05:19 +0200

    print to dot

--- src/Tree/ProgramPath.hs
+++ src/Tree/ProgramPath.hs
@@ -3,6 +3,8 @@ module Tree.ProgramPath where
 
 import GCLParser.GCLDatatype
 import GCLParser.Parser ( parseGCLfile )
+import Data.Foldable
+import Data.Bits
 
 data UniBinTree u b = Leaf
   | Uni u (UniBinTree u b)
@@ -11,14 +13,50 @@ data UniBinTree u b = Leaf
 type ControlPath = UniBinTree Stmt Expr
 
 instance (Show u, Show b) => Show (UniBinTree u b) where
-  show :: UniBinTree u b -> String
-  show = printTree 0
+  -- show :: UniBinTree u b -> String
+  -- show = printTree 0
+  show x = "digraph {" ++ printTreeDot x ++ "}"
 
 printTree :: (Show u, Show b) => Int -> UniBinTree u b -> String
 printTree n Leaf               = replicate (n * 2) ' ' ++ "Leaf\n"
 printTree n (Uni u next)       = replicate (n * 2) ' ' ++ show u ++ "\n" ++ printTree n next
 printTree n (Bin b left right) = replicate (n * 2) ' ' ++ show b ++ "\n" ++ indent ++ "< LEFT >\n" ++ printTree (n + 1) left ++ indent ++ "< RIGHT >\n" ++ printTree (n + 1) right
   where indent = replicate ((n + 1) * 2) ' '
+
+printTreeDot :: (Show u, Show b) => UniBinTree u b -> String
+printTreeDot t = let i = 0 in printTreeDot' i t ++ printTreeDot'' i t
+
+printTreeDot' :: (Show u, Show b) => Int -> UniBinTree u b -> String
+printTreeDot' _ Leaf               = ""
+printTreeDot' i (Uni u next)       = printValueDotLabel i u ++ "\n" ++ printTreeDot' (i + 1) next
+printTreeDot' i (Bin b left right) = printValueDotLabel i b ++ "\n" ++ printTreeDot' (i + 1) left ++ printTreeDot' (i + 1) right
+
+printTreeDot'' :: (Show u, Show b) => Int -> UniBinTree u b -> String
+printTreeDot'' _ Leaf               = ""
+printTreeDot'' i (Uni u next)       = printTreeNextVal i u next ++ printTreeDot'' (i + 1) next
+printTreeDot'' i (Bin b left right) = printTreeNextVal i b left 
+                                   ++ printTreeNextVal i b  right 
+                                   ++ printTreeDot'' (i + 1) left 
+                                   ++ printTreeDot'' (i + 1) right
+
+printTreeNextVal :: (Show v, Show u, Show b) => Int -> v -> UniBinTree u b -> String
+printTreeNextVal i v x | s /= ""   = printValueDot i v ++ " -> " ++ s ++ "\n"
+                       | otherwise = ""
+  where 
+    f x' = case x' of
+      Leaf      -> ""
+      Uni y _   -> printValueDot (i + 1) y
+      Bin y _ _ -> printValueDot (i + 1) y
+    s = f x 
+
+printValueDotLabel :: Show u => Int -> u -> String
+printValueDotLabel i x = printValueDot i x ++ "[label=\"" ++ show x ++ "\"]"
+
+printValueDot :: Show u => Int -> u -> String
+printValueDot i x = show (i + djb2 (show x))
+
+djb2 :: String -> Int
+djb2 = foldl' (\h c -> 33*h `xor` fromEnum c) 5381
 
 orElse :: Maybe t -> Maybe t -> Maybe t
 orElse val@(Just _) _   = val
