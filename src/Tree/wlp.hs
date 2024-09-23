f532cfbcc6feacfd6208f70019cf041b115978d9
MartijnCBV <49876349+MartijnCBV@users.noreply.github.com>
2024-09-23 12:04:30 +0200

    while and trycatch not supported

--- src/Tree/wlp.hs
+++ src/Tree/wlp.hs
@@ -15,15 +15,6 @@ wlp (AAssign s e1 e2)    expr = flip wlp expr $ Assign s $ repBy Var s e1 e2
 wlp (DrefAssign s e)     expr = repBy Dereference s expr e
 wlp (Seq s1 s2)          expr = wlp s1 $ wlp s2 expr
 wlp (IfThenElse e s1 s2) expr = opBinParens (e `opAnd` wlp s1 expr) opOr (OpNeg e `opAnd` wlp s2 expr)
-wlp (While e s)          expr = unrolWhile unrolWhileK (While e s) expr
+wlp (While e s)          expr = error "while not supported by wlp"
 wlp (Block vs s)         expr = expr
-wlp (TryCatch s s1 s2)   expr = wlp s1 expr
-
-unrolWhileK :: Int
-unrolWhileK = 100
-
-unrolWhile :: Int -> Stmt -> Expr -> Expr
-unrolWhile i (While e s) expr | i == 0 = OpNeg e `opImplication` expr
-                              | i > 0  = opBinParens (e `opAnd` wlp s (unrolWhile (i - 1) (While e s) expr)) opOr (OpNeg e `opAnd` expr)
-                              | otherwise = error "bad call to unrollWhile"
-unrolWhile _ _ _              = error "bad call to unroleWhile"
+wlp (TryCatch s s1 s2)   expr = error "try catch not supported by wlp"
