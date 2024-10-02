f78a95b71facfdbaca105b5acf775e845be3cc21
Dennis <dennisowolabi@hotmail.nl>
2024-10-02 13:15:13 +0200

    Add Bool Vars

--- src/Type.hs
+++ src/Type.hs
@@ -1,8 +1,8 @@
-module GDT.Type where
+module Type where
 
 import qualified GCLParser.GCLDatatype as GDT (
   Type,
-  Expr(LitI, LitB, Var, Forall, Exists, BinopExpr, Parens, ArrayElem, OpNeg, SizeOf, Cond),
+  Expr(LitI, LitB, Var, Forall, Exists, BinopExpr, Parens, ArrayElem, OpNeg, SizeOf, RepBy, Cond),
   BinOp(And, Or, Implication, Minus, Plus, Multiply, Divide, LessThan, LessThanEqual, GreaterThan, GreaterThanEqual, Equal))
 import Data.Map (insert, Map, (!), empty)
 import GCLParser.GCLDatatype (Program (..), VarDeclaration (VarDeclaration), Stmt (Block), Type (PType), PrimitiveType (PTInt))
@@ -38,7 +38,7 @@ data TypedExpr
     | Forall             String     TypedExpr
     | Exists             String     TypedExpr
     | SizeOf             TypedExpr
-    -- | RepBy              TypedExpr  TypedExpr   TypedExpr
+    | RepBy              TypedExpr  TypedExpr   TypedExpr
     | Cond               TypedExpr  TypedExpr   TypedExpr
   deriving (Eq, Show)
 
@@ -68,6 +68,7 @@ annotateWithTypes varTypes (GDT.Forall var expr) = Forall var (annotateWithTypes
 annotateWithTypes varTypes (GDT.Exists var expr) = Exists var (annotateWithTypes (addQuantifierVar var varTypes) expr)
 annotateWithTypes varTypes (GDT.ArrayElem array index)   = ArrayElem (annotateWithTypes varTypes array) (annotateWithTypes varTypes index)
 annotateWithTypes varTypes (GDT.BinopExpr op left right) = BinopExpr (convertOp op) (annotateWithTypes varTypes left) (annotateWithTypes varTypes right)
+annotateWithTypes varTypes (GDT.RepBy array index value) = RepBy (annotateWithTypes varTypes array) (annotateWithTypes varTypes index) (annotateWithTypes varTypes value)
 annotateWithTypes varTypes (GDT.Cond cond left right)    = Cond (annotateWithTypes varTypes cond) (annotateWithTypes varTypes left) (annotateWithTypes varTypes right)
 annotateWithTypes _ _ = undefined
 
