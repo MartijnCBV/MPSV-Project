a3637cc6ea999bf1a492ab5f12f6b3ce17576177
Ssoulner <delinda.lortie@gmail.com>
2024-10-03 16:53:56 +0000

    Fix bugs in wlp

--- src/Traverse.hs
+++ src/Traverse.hs
@@ -2,6 +2,23 @@ module Traverse where
 
 import GCLParser.GCLDatatype
 
+orElse :: Maybe a -> a -> a
+orElse Nothing a  = a
+orElse (Just a) _ = a
+
+transformExpr :: (Expr -> Maybe Expr) -> Expr -> Expr
+transformExpr change expr@(Parens e)           = orElse (change expr) (Parens (trans e))                      where trans = transformExpr change
+transformExpr change expr@(ArrayElem a e)      = orElse (change expr) (ArrayElem (trans a) (trans e))         where trans = transformExpr change
+transformExpr change expr@(OpNeg e)            = orElse (change expr) (OpNeg (trans e))                       where trans = transformExpr change
+transformExpr change expr@(BinopExpr op e1 e2) = orElse (change expr) (BinopExpr op (trans e1) (trans e2))    where trans = transformExpr change
+transformExpr change expr@(Forall n e)         = orElse (change expr) (Forall n (trans e))                    where trans = transformExpr change
+transformExpr change expr@(Exists n e)         = orElse (change expr) (Exists n (trans e))                    where trans = transformExpr change
+transformExpr change expr@(SizeOf a)           = orElse (change expr) (SizeOf (trans a))                      where trans = transformExpr change
+transformExpr change expr@(RepBy a e1 e2)      = orElse (change expr) (RepBy (trans a) (trans e1) (trans e2)) where trans = transformExpr change
+transformExpr change expr@(Cond g e1 e2)       = orElse (change expr) (Cond (trans g) (trans e1) (trans e2))  where trans = transformExpr change
+transformExpr change expr@(NewStore e)         = orElse (change expr) (NewStore (trans e))                    where trans = transformExpr change
+transformExpr change expr                      = orElse (change expr) expr
+
 traverseExpr :: (Semigroup m) => (Expr -> m) -> Expr -> m
 traverseExpr get expr@(Parens e)          = get expr <> traverseExpr get e
 traverseExpr get expr@(ArrayElem a e)     = get expr <> traverseExpr get a <> traverseExpr get e
